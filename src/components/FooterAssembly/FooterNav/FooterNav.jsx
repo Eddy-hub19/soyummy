bcfb3b31d7d9c42511ae36ab12a8bdd114f0724e
AM1007 <andrew.motko@gmail.com>
2023-04-05 16:09:35 +0300

    footer added

---
+++ src/components/FooterAssembly/FooterNav/FooterNav.jsx
@@ -0,0 +1,24 @@
+import { scrollToTop } from 'utils/scrollUp';
+import { Link, FooterNavContent } from './NavFooter.styled';
+
+export const FooterNav = () => {
+  return (
+    <FooterNavContent>
+      <Link to="/search?query=&type=ingredients" onClick={scrollToTop}>
+        Ingredients
+      </Link>
+      <Link to="/add" onClick={scrollToTop}>
+        Add recipes
+      </Link>
+      <Link to="/my?page=1" onClick={scrollToTop}>
+        My recipes
+      </Link>
+      <Link to="/favorite" onClick={scrollToTop}>
+        Favorites
+      </Link>
+      <Link to="/shopping-list" onClick={scrollToTop}>
+        Shopping list
+      </Link>
+    </FooterNavContent>
+  );
+};
