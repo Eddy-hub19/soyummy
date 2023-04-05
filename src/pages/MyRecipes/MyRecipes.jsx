c9c2bbcebf0275e25c83c80a2bef6d04570499cc
EduardSekan <sekaneduard@gmail.com>
2023-04-06 00:45:16 +0300

    fix

--- src/pages/MyRecipes/MyRecipes.jsx
+++ src/pages/MyRecipes/MyRecipes.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import { Container } from '../../components/Container/Container';
 import { RecipesList, Thumb } from './MyRecipes.styled';
