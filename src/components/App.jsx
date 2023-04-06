1061256b6735c9c7d4640397058ed39546677f3e
OlenaBielova <belova.elena11@gmail.com>
2023-04-06 14:35:19 +0300

    add recipe page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,7 +32,7 @@ const MainPage = lazy(() =>
 // const Favorites = lazy(() => import('pages/Favorites/Favorites'));
 // const ShopingList = lazy(() => import('pages/ShoppingList/ShoppingList'));
 // const SearchPage = lazy(() => import('pages/SearchPage/SearchPage'));
-// const Recipe = lazy(() => import('pages/Recipe/Recipe'));
+const Recipe = lazy(() => import('pages/RecipePage/RecipePage'));
 
 export const App = () => {
   return (
@@ -87,6 +87,7 @@ export const App = () => {
         <Route path="/" element={<UserSharedLayout />} />
         {/* <Route path="/signin" element={<AuthNav />} />
         <Route path="/register" element={<AuthNav />} /> */}
+        <Route path="/recipes/:recipeId" element={<Recipe />} />
       </Routes>
 
       {/* <Route path="/categories" element={<Categories />}>
@@ -96,7 +97,7 @@ export const App = () => {
       {/* <Route path="/favorite" element={<Favorites />} />
       <Route path="/shopping-list" element={<ShopingList />} />
       <Route path="/search" element={<SearchPage />} />
-      <Route path="/recipe/:recipeId" element={<Recipe />} />
+      
       <Route path="*" element={<Error />} />  */}
     </ThemeProvider>
   );
---
+++ src/components/RecipePreparation/RecipePreparation.jsx
@@ -0,0 +1,23 @@
+
+
+const RecipePreparation = () => {
+    return (
+        <div>
+            <h3>Recipe Preparation</h3>
+            
+            {/* <ol>
+                <li>
+                    <p>Melt the butter in a thick-based pan and gently cook the onion without colour until it is soft.</p>
+                </li>
+                <li>
+                    <p>Add the rice and stir to coat all the grains in the butter.</p>
+                </li>
+                <li>
+                    <p>Add the wine and cook gently stirring until it is absorbed.</p>
+                </li>
+            </ol> */}
+        </div>
+  );
+};
+
+export default RecipePreparation;
