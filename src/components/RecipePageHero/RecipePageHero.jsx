200d78ab5e08fd584fd165d7d13dc447a7948601
AM1007 <andrew.motko@gmail.com>
2023-04-09 14:59:11 +0300

    header added to container

--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -2,49 +2,47 @@ import React from 'react';
 import { ButtonSkew } from 'components/ButtonSkew/ButtonSkew';
 import sprite from '../../images/sprite.svg';
 import {
-    RecipeHeroContainer,
-    RecHeroCont,
+  RecipeHeroContainer,
   HeroTitle,
-    HeroText,
-    CookingTime
+  HeroText,
+  CookingTime,
 } from './RecipePageHero.styled';
 // import { Loader } from 'components/Loader/Loader';
 
 const RecipePageHero = ({ recipe }) => {
-    console.log(recipe);
-    const { title, description, time, ingredients } = recipe;
-    console.log(ingredients);
+  console.log(recipe);
+  const { title, description, time, ingredients } = recipe;
+  console.log(ingredients);
 
-    return (
-        <>
-            {/* <RecHeroCont> */}
-            <RecipeHeroContainer>
-                
-            {/* <Loader></Loader> */}
-            <HeroTitle>{title}</HeroTitle>
-            <HeroText>{description}</HeroText>
-            <ButtonSkew
-                type="button"
-                text="Add to favorite recipes"
-                styled="other"
-                location="recipes"
-                // fn={addtoFavorite}
-                />
+  return (
+    <>
+      {/* <RecHeroCont> */}
+      <RecipeHeroContainer>
+        {/* <Loader></Loader> */}
+        <HeroTitle>{title}</HeroTitle>
+        <HeroText>{description}</HeroText>
+        <ButtonSkew
+          type="button"
+          text="Add to favorite recipes"
+          styled="other"
+          location="recipes"
+          // fn={addtoFavorite}
+        />
 
-            {time !== '' ? (
-            <CookingTime>
-                <svg>
-                <use href={sprite + `#icon-clock`} />
-                </svg>
-                <span>{time + ` min`}</span>
-            </CookingTime>
-            ) : (
-            <CookingTime></CookingTime>
-                )}
-                
-                </RecipeHeroContainer>
-                {/* </RecHeroCont> */}
-            </>
-    )};
+        {time !== '' ? (
+          <CookingTime>
+            <svg>
+              <use href={sprite + `#icon-clock`} />
+            </svg>
+            <span>{time + ` min`}</span>
+          </CookingTime>
+        ) : (
+          <CookingTime></CookingTime>
+        )}
+      </RecipeHeroContainer>
+      {/* </RecHeroCont> */}
+    </>
+  );
+};
 
 export default RecipePageHero;
