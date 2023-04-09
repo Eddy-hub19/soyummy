62bb868535afed128acee915d34bc239b0917046
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-04-09 21:38:43 +0300

    123

--- src/service/API/CategoriesAPI.js
+++ src/service/API/CategoriesAPI.js
@@ -1,16 +1,6 @@
 import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_BACK;
 
-// export const getRecipeById = async (token, id) => {
-//     const config = {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   };
-//   const response = await axios.get(`/recipes/${id}`, config);
-//   return response.data;
-// };
-
 export const getCategoryListAPI = async (token) => {
     const config = {
     headers: {
