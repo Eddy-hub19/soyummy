2549ff88dcd567824141451538978659768a0958
Alexander <Alexyaroshovets300687@gmail.com>
2023-04-09 19:51:11 +0300

    getPopular

--- src/components/AddRecipePopular/AddRecipePopular.jsx
+++ src/components/AddRecipePopular/AddRecipePopular.jsx
@@ -1,15 +1,24 @@
-import { SubTitle } from 'components/SubTitle/SubTitle';
-import {
-  //   PopularItem,
-  PopularRecipe,
-  PopularSection,
-  //   RecepiImg,
-  //   RecipeText,
-  //   RecipeTitle,
-} from 'pages/AddRecipe/addRecipe.styled';
+// import { SubTitle } from 'components/SubTitle/SubTitle';
+import { getPopularListAPI } from 'service/axios/axios';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import //   PopularItem,
+//   PopularRecipe,
+//   PopularSection,
+//   RecepiImg,
+//   RecipeText,
+//   RecipeTitle,
+'pages/AddRecipe/addRecipe.styled';
 // import { useSelector } from 'react-redux';
 // import { Link } from 'react-router-dom';
-
+import {
+  StyledPopTitle,
+  StyledPopitem,
+  ImgWrapper,
+  TextWrapper,
+  ItemTitle,
+  Description,
+} from './AddPopular.styled';
 export const AddRecipePopular = ({ isDesktop, isTablet, localTheme }) => {
   //   const popularList = popularRecepis.map(
   //     ({ idMeal, strMealThumb, strInstructions, strMeal }) => (
@@ -26,25 +35,57 @@ export const AddRecipePopular = ({ isDesktop, isTablet, localTheme }) => {
 
   //     )
   //   );
+
+  const [popularOpt, setPopularOpt] = useState([]);
+
+  useEffect(() => {
+    const handleEffect = async () => {
+      try {
+        const popularOptObj = await getPopularListAPI();
+        const popelarArray = popularOptObj.result;
+        const firstFour = popelarArray.slice(0, 4);
+        console.log(firstFour);
+
+        setPopularOpt([...firstFour]);
+
+        // if (allCategories.length === 0) {
+        //   return;
+        // }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleEffect();
+  }, []);
+
+  //{ _id, thumb, strInstructions, title }
+
   return (
-    <PopularSection isDesktop={isDesktop}>
-      <PopularRecipe>
-        <SubTitle text="Popular recipe" />
-        {/* direction={isTablet ? 'horizontal' : 'vertical'}
-        style=
-        {isTablet
-          ? { padding: '20px 0' }
-          : { height: '480px', width: '340px', padding: '10px 0' }}
-        autoplay=
-        {{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        centeredSlides={false}
-        slidesPerView={isTablet ? 2 : 4}
-        spaceBetween={isTablet ? 30 : 10}
-        freeMode={true} */}
-      </PopularRecipe>
-    </PopularSection>
+    <div>
+      <StyledPopTitle>Popular recipe</StyledPopTitle>
+      {popularOpt.length > 0 && (
+        <ul>
+          {popularOpt.map(({ _id, thumb, instructions, title }) => {
+            const link = `/recipe/${_id}`;
+            return (
+              <StyledPopitem key={_id}>
+                <Link to={link}>
+                  <ImgWrapper>
+                    <img src={thumb} alt={title} />
+                  </ImgWrapper>
+                  <TextWrapper>
+                    <ItemTitle>
+                      {title.length > 30 ? title.slice(0, 35) + '...' : title}
+                    </ItemTitle>
+                    <Description>{instructions.slice(0, 80)}...</Description>
+                  </TextWrapper>
+                </Link>
+              </StyledPopitem>
+            );
+          })}
+        </ul>
+      )}
+      {popularOpt.length === 0 && <p>No popular recipes</p>}
+    </div>
   );
 };
