8286ecc9775c60c9f11903fb3e8d81bbe2f09578
Viktoriia <viktoriia.zahorodnia@gmail.com>
2023-04-11 01:00:34 +0200

    fix

--- src/components/ShoppingList/ShoppingListTabl.jsx
+++ src/components/ShoppingList/ShoppingListTabl.jsx
@@ -39,6 +39,7 @@ const ShoppingListTabl = () => {
   useEffect(() => {
     dispatcher(fetchShoppingList());
     setShopItems(storageItems);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatcher]);
 
   useEffect(() => {
