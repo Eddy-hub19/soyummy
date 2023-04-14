db97336a6300836d7d6fd2f053d59512db0ba14f
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-04-14 22:01:13 +0300

    qwe

--- src/components/HeaderUserBar/HeaderProfileLogOutModal/EditProfileModal/EditProfileModal.jsx
+++
@@ -1,9 +0,0 @@
-const EditProfileModal = () => {
-  return (
-    <div>
-      <p>Edit Profile Modal</p>
-    </div>
-  );
-};
-
-export default EditProfileModal;
--- src/components/HeaderUserBar/UserSharedLayout.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const DivWrapper = styled.div`
-  display: flex;
-  border: none;
-  margin-left: 500px;
-`;
