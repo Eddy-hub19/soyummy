595e7e2426dcd2aab99b2748b16a1ae36fb250f1
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2023-04-17 17:34:57 +0300

    fix fix fix

--- src/components/HeaderAssembly/HeaderUserLogo/HeaderUserLogo.jsx
+++ src/components/HeaderAssembly/HeaderUserLogo/HeaderUserLogo.jsx
@@ -23,7 +23,9 @@ export const HeaderUserLogo = ({ avatarUrl = userAvatar, color = "light" }) => {
   };
 
   const handleBlur = () => {
-    setShowUserMenu(false);
+    setTimeout(() => {
+      setShowUserMenu(false);
+    }, 300);
   };
 
   useEffect(() => {
