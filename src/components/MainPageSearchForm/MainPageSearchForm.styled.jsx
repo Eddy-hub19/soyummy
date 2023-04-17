6edf01a495caa817aa6598ff8773de91f60d6b87
Marina <sheremetamaryna@gmail.com>
2023-04-17 18:09:05 +0200

    upd main search

--- src/components/MainPageSearchForm/MainPageSearchForm.styled.jsx
+++ src/components/MainPageSearchForm/MainPageSearchForm.styled.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const SearchBtn = styled.button`
   display: flex;
@@ -7,36 +7,36 @@ export const SearchBtn = styled.button`
   width: fit-content;
   white-space: nowrap;
   box-sizing: border-box;
-  font-family: ${p => p.theme.fonts.main};
-  font-weight: ${p => p.theme.fontWeights[0]};
+  font-family: ${(p) => p.theme.fonts.main};
+  font-weight: ${(p) => p.theme.fontWeights[0]};
   line-height: 1.35;
   font-size: 14px;
-    position: absolute;
-    left: 182px;
+  position: absolute;
+  left: 182px;
 
-  color: ${p =>
-    p.styled === 'black'
+  color: ${(p) =>
+    p.styled === "black"
       ? p.theme.colors.btnTextLight
-      : p.styled === 'olive'
+      : p.styled === "olive"
       ? p.theme.colors.btnTextLight
-      : p.styled === 'other'
+      : p.styled === "other"
       ? p.theme.colors.mainBtnText
-      : p.styled === 'transparent'
+      : p.styled === "transparent"
       ? p.theme.colors.mainDark
-      : 'red'};
+      : "red"};
 
-  padding:15.7px 31.72px;
+  padding: 15.7px 31.72px;
 
-  background-color: ${p =>
-    p.styled === 'black'
+  background-color: ${(p) =>
+    p.styled === "black"
       ? p.theme.colors.recipeBlockBtnBg
-      : p.styled === 'olive'
+      : p.styled === "olive"
       ? p.theme.colors.mainAccent
-      : p.styled === 'other'
-      ? 'transparent'
-      : p.styled === 'transparent'
-      ? 'transparent'
-      : 'red'};
+      : p.styled === "other"
+      ? "transparent"
+      : p.styled === "transparent"
+      ? "transparent"
+      : "red"};
 
   outline: none;
   border: none;
@@ -50,12 +50,12 @@ export const SearchBtn = styled.button`
 
   border: ${({ styled, theme }) => {
     switch (styled) {
-      case 'other':
+      case "other":
         return `1px solid ${theme.colors.mainAccent}`;
-      case 'transparent':
+      case "transparent":
         return `1px solid ${theme.colors.btnTextLight}`;
       default:
-        return '1px solid transparent';
+        return "1px solid transparent";
     }
   }};
 
@@ -67,53 +67,52 @@ export const SearchBtn = styled.button`
   &:hover {
     color: ${({ styled, theme }) => {
       switch (styled) {
-        case 'black':
+        case "black":
           return theme.colors.mainSerchHoverText;
-        case 'olive':
+        case "olive":
           return theme.colors.searchDropDownBg;
-        case 'other':
+        case "other":
           return theme.colors.btnTextLight;
-        case 'transparent':
+        case "transparent":
           return theme.colors.mainAccent;
         default:
-          return 'red';
+          return "red";
       }
     }};
     background-color: ${({ styled, theme }) => {
       switch (styled) {
-        case 'black':
+        case "black":
           return theme.colors.footerSMlinks;
-        case 'olive':
+        case "olive":
           return theme.colors.searchFormHoverBtn;
-        case 'transparent':
-          return 'transparent';
-        case 'other':
+        case "transparent":
+          return "transparent";
+        case "other":
           return theme.colors.mainAccent;
         default:
-          return 'red';
+          return "red";
       }
     }};
-    border: ${p =>
-      p.styled === 'black'
+    border: ${(p) =>
+      p.styled === "black"
         ? `1px solid ${p.theme.colors.mainAccent}`
-        : p.styled === 'olive'
+        : p.styled === "olive"
         ? p.theme.borders.mainNavBord
-        : p.styled === 'transparent'
+        : p.styled === "transparent"
         ? `1px solid ${p.theme.colors.mainAccent}`
-        : '1px solid transparent'};
+        : "1px solid transparent"};
   }
 
   @media screen and (min-width: 768px) {
-      font-size: 16px;
-      line-height: 1.5;
-      padding: 17px 52px;
+    font-size: 16px;
+    line-height: 1.5;
+    padding: 17px 52px;
     min-width: 130px;
     position: absolute;
     left: 228px;
   }
 
   @media screen and (min-width: 1440px) {
-
     padding: 23px 52px;
     min-width: 160px;
     position: absolute;
@@ -123,44 +122,44 @@ export const SearchBtn = styled.button`
 
 export const SearchInput = styled.input`
   box-sizing: border-box;
-  border: ${p => p.theme.borders.mainSearchInput};
-  background-color: ${p => p.theme.colors.mainSearchInput};
-  width:295px;
-  height:52px;
+  border: ${(p) => p.theme.borders.mainSearchInput};
+  background-color: ${(p) => p.theme.colors.mainSearchInput};
+  width: 295px;
+  height: 52px;
   padding-left: 30px;
-  font-family: ${p => p.theme.fonts.main};
-  font-weight: ${p => p.theme.fontWeights[0]};
-  font-size:16px;
-  line-height: ${p => p.theme.lineHeights.subheader};
+  font-family: ${(p) => p.theme.fonts.main};
+  font-weight: ${(p) => p.theme.fontWeights[0]};
+  font-size: 16px;
+  line-height: ${(p) => p.theme.lineHeights.subheader};
   border-radius: 18px 44px;
 
-  color: ${p => p.theme.colors.disabledGrey};
+  color: ${(p) => p.theme.colors.disabledGrey};
   &:hover {
     border-color: rgba(35, 38, 42, 0.2);
-    color: ${p => p.theme.colors.mainBlack};
+    color: ${(p) => p.theme.colors.mainBlack};
     outline: none;
     outline: none;
   }
   &:active {
     border-color: rgba(35, 38, 42, 0.2);
-    color: ${p => p.theme.colors.mainBlack};
+    color: ${(p) => p.theme.colors.mainBlack};
     outline: none;
     outline: none;
   }
   &:focus {
     border-color: rgba(35, 38, 42, 0.2);
-    color: ${p => p.theme.colors.mainBlack};
+    color: ${(p) => p.theme.colors.mainBlack};
     outline: none;
     outline: none;
   }
 
   @media screen and (min-width: 768px) {
-    width:362px;
-    height:59px;
+    width: 362px;
+    height: 59px;
   }
   @media screen and (min-width: 1440px) {
- width:510px;
- height:70px;
+    width: 510px;
+    height: 70px;
   }
 `;
 
@@ -172,38 +171,38 @@ export const Form = styled.form`
   height: 100%;
   justify-content: center;
   align-items: start;
-  height:52px;
-  width:290px;
-  z-index:3;
+  height: 52px;
+  width: 290px;
+  z-index: 3;
 
   @media screen and (min-width: 375px) {
-    padding-left: 32px
+    padding-left: 32px;
   }
 
   @media screen and (min-width: 768px) {
-    width:362px;
-    height:59px;
+    width: 362px;
+    height: 59px;
   }
   @media screen and (min-width: 1440px) {
-  width:510px;
-  height:70px;
+    width: 510px;
+    height: 70px;
   }
 `;
 
 export const Wrapper = styled.div`
-box-sizing:border-box;
+  box-sizing: border-box;
 
   position: absolute;
   top: 662px;
   left: 24px;
 
   @media screen and (min-width: 768px) {
-  top: 386px;
-  left:0px;
+    top: 386px;
+    left: 0px;
   }
   @media screen and (min-width: 1440px) {
-  margin-left: 100px;
-  top: 462px;
-  left:0px;
+    margin-left: 100px;
+    top: 462px;
+    left: -100px;
   }
 `;
